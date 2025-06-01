a7e92a604a7fe7c9572303dafdc9514b88977a33
Anmar <117080591+AnmarX@users.noreply.github.com>
2025-06-01 20:24:16 +0300

    download jenkins

---
+++ DownloadJenkins.sh
@@ -0,0 +1,22 @@
+# download package manager from this url 
+https://adoptium.net/installation/linux/
+
+
+# then jenkins from here 
+https://www.jenkins.io/doc/book/installing/linux/
+
+# check if jenkins is runinng from root
+systemctl --full status jenkins
+
+# to start or stop the service
+systemctl start jenkins
+systemctl stop jenkins
+
+# to get the password of the url to sign into the service
+journalctl -u jenkins
+
+#url of the service
+localhost:8080
+
+#edit jenkins file
+systemctl edit jenkins
